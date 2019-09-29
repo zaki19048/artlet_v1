6657e48c0bc21d92dc7575ecdf7cbb822b0ecdb6
Suryank <suryank19019@iiitd.ac.in>
2019-09-29 15:53:53 +0530

    Manga Reader Base ready

--- app/src/main/java/com/example/artlet_v1/MainActivity.java
+++ app/src/main/java/com/example/artlet_v1/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.artlet_v1;
 import androidx.appcompat.app.AppCompatActivity;
 //import DatabaseHelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper db;
+    MangaReader mr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.db = new DatabaseHelper(getApplicationContext());
+    }
 
+    public void testManga(View view)
+    {
+        Intent newIntent = new Intent(this, MangaReader.class);
+        startActivity(newIntent);
     }
 }
