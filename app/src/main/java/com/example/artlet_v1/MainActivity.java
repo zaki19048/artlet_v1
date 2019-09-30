ffa73450c0185b84801d51aebdad8281c4bdd69b
zaki19048 <53988543+zaki19048@users.noreply.github.com>
2019-09-30 15:59:48 +0530

    Revert "Search Fragment"

--- app/src/main/java/com/example/artlet_v1/MainActivity.java
+++ app/src/main/java/com/example/artlet_v1/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.artlet_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 //import DatabaseHelper;
 
 import android.os.Bundle;
@@ -14,12 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //this.db = new DatabaseHelper(getApplicationContext());
-        Search_Fragment searchFragment=new Search_Fragment();
-        FragmentManager mFragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.mainactivity,searchFragment,"Search_Fragment");
-        fragmentTransaction.commit();
+        this.db = new DatabaseHelper(getApplicationContext());
 
     }
 }
