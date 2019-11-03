88628957cea79437f7008a779c6436f7ab3ffe26
reshan9b11 <reshanofficial@gmail.com>
2019-11-03 19:02:16 +0530

    Leader Board "1-Calling leader Board on skip button 2- change the  query on LeaderBoardFragmentt class (on which basis we want to show LeaderBoard" 3- when code merge we call the LeaderBoard from Navigation bar (already done by Prateek)

--- app/src/main/java/com/example/artlet_v1/LeaderBoardFragmentt.java
+++ app/src/main/java/com/example/artlet_v1/LeaderBoardFragmentt.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.artlet_v1.R;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class LeaderBoardFragmentt extends AppCompatActivity {
 
@@ -56,6 +59,7 @@ public class LeaderBoardFragmentt extends AppCompatActivity {
 
 
     ListView lv;
+    CircleImageView first,second,third;
 
 
 
@@ -69,12 +73,27 @@ public class LeaderBoardFragmentt extends AppCompatActivity {
         Cursor c = db.rawQuery("SELECT content.id as content_id , genre.name as genre_name, user.name as user_name, title, type, likes ,content.created_at as created_at,file FROM content INNER JOIN user ON content.author_id = user.id INNER JOIN genre ON content.genre_id = genre.id ORDER BY content.id DESC", null);
         final String[] title=new String[c.getCount()] ;
         lv = (ListView) findViewById(R.id.list_leader);
+        first=(CircleImageView)findViewById(R.id.imageview2);
+        second=(CircleImageView)findViewById(R.id.imageview1);
+        third=(CircleImageView)findViewById(R.id.imageview3);
+        Drawable myDrawabl1 = getResources().getDrawable(R.drawable.manga);
+        Drawable myDrawabl2 = getResources().getDrawable(R.drawable.artlet);
+        Drawable myDrawabl3 = getResources().getDrawable(R.drawable.epub);
         if(c!=null && c.moveToFirst())
         {
             int i=0;
             do {
                 String uname = c.getString(c.getColumnIndex("user_name"));
                 title[i]=uname;
+                if(i==0){
+                    first.setImageDrawable(myDrawabl2);
+                }
+                if(i==1){
+                    second.setImageDrawable(myDrawabl1);
+                }
+                if(i==2){
+                    third.setImageDrawable(myDrawabl3);
+                }
                 i=i+1;
             }while(c.moveToNext());
         }
